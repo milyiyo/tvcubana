773ec545d373dcb5466aa56782ddfe074fdb85b5
Alberto Carmona <carmo@aleph.engineering>
2020-09-06 17:45:17 -0400

    Remove unneeded import

--- lib/widgets/MoviesList.dart
+++ lib/widgets/MoviesList.dart
@@ -2,8 +2,6 @@ import 'package:tvcubana/models/Channel.dart';
 import 'package:tvcubana/models/ProgramItem.dart';
 import 'package:tvcubana/utils.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert' as convert;
-import 'package:http/http.dart' as http;
 import 'package:chips_choice/chips_choice.dart';
 
 import 'ProgramItemCard.dart';
